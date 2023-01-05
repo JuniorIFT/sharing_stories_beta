feac92a8869aba51d8bdc8f374fd9a72dac0dd60
JuniorIFT <adilsonjuniorptu@gmail.com>
2023-01-05 15:02:50 -0300

    first commit

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\JsonResponse;
 use Laravel\Socialite\Facades\Socialite;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Facades\Auth;
@@ -15,14 +16,20 @@ class AuthController extends Controller
 
     public function loginCallback($driver, UserController $user)
     {
-        $user_data = Socialite::driver($driver)->user();
-        $user->store($user_data, $driver);
-        return redirect('/dashboard');
+        if ($user_data = Socialite::driver($driver)->user()) {
+            $user->store($user_data, $driver);
+            return redirect('/dashboard');
+        } else {
+            return new JsonResponse([
+                'message' => 'Erro ao fazer login com o ' . $driver,
+            ], 500);
+        }
     }
 
     public function logout()
     {
         Auth::logout();
         Session::flush('user_id');
+        return redirect('/');
     }
 }
